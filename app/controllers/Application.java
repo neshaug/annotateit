62803c550b00f2408ef1d144f12f38265a3b68ad
Oyvind Neshaug <oyvind@neshaug.net>
2011-04-16 00:44:55 +0200

    Add users and notes controllers

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,7 +10,8 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        List<User> users = User.all().fetch();
+        renderJSON(users);
     }
 
 }
