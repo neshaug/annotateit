0aaf23de8d839c23e0146b9535d7d454379eda43
Martin Polden <martin.polden@gmail.com>
2011-04-16 14:51:25 +0200

    Basic controller for notes

--- app/controllers/Notes.java
+++ app/controllers/Notes.java
@@ -1,5 +1,23 @@
 package controllers;
 
-public class Notes {
+import models.Note;
+import play.mvc.Controller;
 
+import java.util.List;
+
+public class Notes extends Controller {
+
+    public static void save(Note note) {
+        note.save();
+    }
+
+    public static void show(Long id) {
+        Note note = Note.findById(id);
+        renderJSON(note);
+    }
+
+    public static void all() {
+        List<Note> notes = Note.all().fetch();
+        renderJSON(notes);
+    }
 }
