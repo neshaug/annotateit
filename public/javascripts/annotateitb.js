5d44e5f438d1cbab45ac6baeef3df75780bdb6a9
Oyvind Neshaug <oyvind@neshaug.net>
2011-04-17 18:24:59 +0200

    Add initial UI logic to add notes

--- public/javascripts/annotateitb.js
+++ public/javascripts/annotateitb.js
@@ -1,9 +1,47 @@
-$(document).ready(function() {
+/*global
+$, window, document, console
+*/
+
+/*jslint
+nomen: false
+*/
+$(document).ready(function () {
     $.ajax({
-        url: "http://127.0.0.1:9000/notes/all",
-        dataType: "jsonp",
-        success: function(data) {
+        url: 'http://127.0.0.1:9000/notes/all',
+        dataType: 'jsonp',
+        success: function (data) {
             console.log(data);
         }
     });
+
+    $('#dialog').dialog();
+
+    function Note() {
+        this._id = 0;
+        this._content = null;
+        this.x = 0;
+        this.y = 0;
+        this._xpath = null;
+    }
+
+    function NoteView() {
+        this._dialog = null;
+    }
+
+    NoteView.prototype.render = function (note) {
+        var div = document.createElement("div");
+        this._dialog = $(div).dialog({
+            position: [note.x, note.y]
+        });
+    };
+
+    function clickHandler(e) {
+        e.target;
+        var note = new Note();
+        note.x = e.clientX;
+        note.y = e.clientY;
+        new NoteView().render(note);
+    }
+
+    window.onclick = clickHandler;
 });
